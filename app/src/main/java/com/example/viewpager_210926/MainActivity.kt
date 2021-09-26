774686565ca774e2bd5006f51fbfee9662f31f61
이진호 <spris1513@gmail.com>
2021-09-26 13:55:25 +0900

    탭에이아웃 배치 + 뷰페이저와 연결

--- app/src/main/java/com/example/viewpager_210926/MainActivity.kt
+++ app/src/main/java/com/example/viewpager_210926/MainActivity.kt
@@ -16,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         mvpa = MainViewPagerAdapter( supportFragmentManager )
         mainViewPager.adapter = mvpa
 
+
+
+//        탭레이아웃을 > 뷰페이저와 연계
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
     }
 }
