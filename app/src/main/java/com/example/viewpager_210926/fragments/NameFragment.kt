c8df2ad4fd640cff1f75d069dc104bdd5c22c05f
이진호 <spris1513@gmail.com>
2021-09-26 12:53:38 +0900

    이름, 나이 프래그먼트 추가

--- app/src/main/java/com/example/viewpager_210926/fragments/NameFragment.kt
+++ app/src/main/java/com/example/viewpager_210926/fragments/NameFragment.kt
@@ -7,22 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.viewpager_210926.R
 
-class GreetingFragment : Fragment(){
+class NameFragment : Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_greeting,container,false)
+        return inflater.inflate(R.layout.fragment_name,container,false)
 
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //        나중에 이벤트 처리 작성
-
-
+//        나중에 이벤트 처리 작성
     }
 
 }
