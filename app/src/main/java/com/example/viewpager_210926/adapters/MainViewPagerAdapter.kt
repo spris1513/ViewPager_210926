d844bc06f2322c2c57ab8f6e8e37d4a440b1f329
이진호 <spris1513@gmail.com>
2021-09-26 13:37:28 +0900

    프래그먼트 페이징 담당 어댑터 작성중  - 상속 / 생성자 > 기초작업  - 필수 구현 함수 2개 오버라이딩까지만함, 내부작업해야함

---
+++ app/src/main/java/com/example/viewpager_210926/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.viewpager_210926.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
+}
